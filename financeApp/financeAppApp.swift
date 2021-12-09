436e22e75182ebad5ffbb8e1ee9b8302dbfce011
Riya Jain <riyaj5246@gmail.com>
2021-12-09 13:50:32 -0600

    Initial Commit

---
+++ financeApp/financeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeAppApp.swift
+//  financeApp
+//
+//  Created by Riya Jain on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct financeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
